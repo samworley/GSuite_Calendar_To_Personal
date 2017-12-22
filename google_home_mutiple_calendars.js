94ef6ea1c1371cbb3a1481fe5982ed4b2a76f4f3
Sam Worley <samworley@users.noreply.github.com>
2017-12-22 00:27:44 +0000

    Update and rename google_home_mutiple_calendars.txt to google_home_mutiple_calendars.js
    Fixed check on size of name object, added event description, delete existing events, add events for next 7 days

---
+++ google_home_mutiple_calendars.js
@@ -0,0 +1,39 @@
+function addToGoogleHome() {
+  
+    var calendarsWithName = {
+        "work": "work_email",
+        "school": "school_email",
+        "podcast" : "podcast_calendar_id"
+        "Roy": "Roy's_email"
+    };
+    var google_home_email = "personal_email/google_home_email";
+    var googleHomeCalendar = CalendarApp.getCalendarById(google_home_email);
+    var currentDate = new Date();
+    var num_events = 0;
+
+    for (var i in calendarsWithName) {
+        var calendarToCopy = CalendarApp.getCalendarById(calendarsWithName[i]);
+        var name = i;
+        if (Object.keys(calendarsWithName).length === 1) {
+            name = "";
+        }
+        //loop through next 7 days
+        for (var day = 1; day <= 7; day++) {
+            currentDate.setDate(currentDate.getDate() + 1);
+            var oldEvents = googleHomeCalendar.getEventsForDay(currentDate);
+            var events = calendarToCopy.getEventsForDay(currentDate);
+          
+            //delete old events first
+            for (var j in oldEvents) { 
+              oldEvents[j].deleteEvent();
+            }
+            //add new events
+            for (var k = 0; k < events.length; k++) { 
+              googleHomeCalendar.createEvent(name + " " + events[k].getTitle(), events[k].getStartTime(), events[k].getEndTime(), {description: events[k].getDescription()});
+            }
+            num_events = num_events + events.length;
+        }
+    }
+
+    GmailApp.sendEmail(google_home_email, "copied" + num_events + " events.", "");
+}
